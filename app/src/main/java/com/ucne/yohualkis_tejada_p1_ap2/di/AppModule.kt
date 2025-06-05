206b4a61f6ffd63559195b7d117cffa6103a4cd4
Yohualkis <Yohualkis99@gmail.com>
2025-06-04 21:43:37 -0400

    Se creo el AppModule.kt

---
+++ app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.ucne.yohualkis_tejada_p1_ap2.di
+
+import android.content.Context
+import androidx.room.Room
+import com.ucne.yohualkis_tejada_p1_ap2.data.local.database.Parcial1Db
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule{
+    @Provides
+    @Singleton
+    fun database(@ApplicationContext contexto: Context) =
+        Room.databaseBuilder(
+            context = contexto,
+            klass = Parcial1Db::class.java,
+            name = "Parcial1.db"
+        ).fallbackToDestructiveMigration().build()
+
+    @Provides
+    fun provideTareaDao(parcial1Db: Parcial1Db) = parcial1Db.TareaDao()
+}
