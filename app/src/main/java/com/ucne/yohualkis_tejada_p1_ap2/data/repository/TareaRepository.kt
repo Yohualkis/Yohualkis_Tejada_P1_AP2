bea5fdfc6b7a5c0871db846f623982e5435da37e
Yohualkis <Yohualkis99@gmail.com>
2025-06-04 21:41:10 -0400

    Se creo el repositorio

---
+++ app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/data/repository/TareaRepository.kt
@@ -0,0 +1,17 @@
+package com.ucne.yohualkis_tejada_p1_ap2.data.repository
+
+import com.ucne.yohualkis_tejada_p1_ap2.data.local.dao.TareaDao
+import com.ucne.yohualkis_tejada_p1_ap2.data.local.entities.TareaEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TareaRepository @Inject constructor(
+    private val dao: TareaDao
+) {
+    suspend fun save(tarea: TareaEntity) = dao.save(tarea)
+    suspend fun find(id: Int?): TareaEntity? = dao.find(id)
+    suspend fun delete(tarea: TareaEntity) = dao.delete(tarea)
+    fun getAll(): Flow<List<TareaEntity>> = dao.getAll()
+}
