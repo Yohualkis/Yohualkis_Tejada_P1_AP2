414e6746078f620594b3c8dbaa82164888e39a61
Yohualkis <Yohualkis99@gmail.com>
2025-06-04 21:42:36 -0400

    Se creo el list Screen

---
+++ app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/presentation/tarea/TareaListScreen.kt
@@ -0,0 +1,105 @@
+package com.ucne.yohualkis_tejada_p1_ap2.presentation.tarea
+
+import com.ucne.yohualkis_tejada_p1_ap2.data.local.entities.TareaEntity
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.ucne.yohualkis_tejada_p1_ap2.presentation.composables.TopBarGenerica
+
+@Composable
+fun TareaListScreen(
+    viewModel: TareaViewModel = hiltViewModel(),
+    goToTarea: (Int?) -> Unit,
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    DesplegarListado(
+        uiState = uiState,
+        goToTarea = { goToTarea(it) },
+        evento = { viewModel::onEvent },
+        onDeleteClick = { viewModel.onEvent(TareaEvent.Delete(it)) },
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DesplegarListado(
+    uiState: TareaUiState,
+    goToTarea: (Int?) -> Unit,
+    onDeleteClick: (TareaEntity) -> Unit,
+    evento: (TareaEvent) -> Unit
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    goToTarea(0)
+                }
+            ) {
+                Icon(Icons.Filled.Add, "")
+            }
+        },
+        topBar = {
+            TopBarGenerica(
+                goBack = {},
+                titulo = "Listado de Tareas"
+            )
+        }
+    ) { innerPadding ->
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .padding(innerPadding)
+        ) {
+            items(uiState.listaTareas){ tarea ->
+                TareaRow(
+                    tarea = tarea,
+                    onEditClick = { goToTarea(tarea.tareaId) },
+                    onDeleteClick = { onDeleteClick(tarea) },
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewListado() {
+    DesplegarListado(
+        uiState = TareaUiState(
+            listaTareas = listOf(
+                TareaEntity(
+                    tareaId = 1,
+                    descripcion = "Descripcion",
+                    tiempo = 1
+                ),
+                TareaEntity(
+                    tareaId = 2,
+                    descripcion = "Descripcion",
+                    tiempo = 2
+                ),
+            )
+        ),
+        onDeleteClick = {},
+        evento = {},
+        goToTarea = {},
+    )
+}
