bbb45b6c40bec2fd8b85a578c47563f505e1e728
Yohualkis <Yohualkis@DESKTOP-FKD43PD>
2025-08-31 21:14:15 -0400

    Se modifica el TopBarGenerica para que se pueda ocultar el boton de volver atras y en su lugar se muestre un icono de home.

--- app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/ui/presentation/composables/TopBarGenerica.kt
+++ app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/ui/presentation/composables/TopBarGenerica.kt
@@ -3,12 +3,12 @@ package com.ucne.yohualkis_tejada_p1_ap2.ui.presentation.composables
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -17,18 +17,30 @@ import androidx.compose.ui.unit.sp
 fun TopBarGenerica(
     goBack: () -> Unit,
     titulo: String,
-){
-    Row (
+    backButtonVisible: Boolean = true
+) {
+    Row(
         verticalAlignment = Alignment.CenterVertically
-    ){
-        IconButton(
-            onClick = { goBack() },
-            modifier = Modifier
-        ) {
-            Icon(
-                Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = ""
-            )
+    ) {
+        if (backButtonVisible) {
+            IconButton(
+                onClick = { goBack() },
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = ""
+                )
+            }
+        } else {
+            IconButton(
+                onClick = { },
+                enabled = false
+            ) {
+                Icon(
+                    Icons.Default.Home,
+                    contentDescription = ""
+                )
+            }
         }
         Text(
             text = titulo,
@@ -40,6 +52,6 @@ fun TopBarGenerica(
 
 @Preview(showBackground = true)
 @Composable
-fun Preview(){
+fun Preview() {
     TopBarGenerica({}, "Prueba")
 }
