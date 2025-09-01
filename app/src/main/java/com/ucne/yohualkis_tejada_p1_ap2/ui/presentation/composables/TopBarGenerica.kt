312ff770b7f323fba27737a36435b2056245615d
Yohualkis <Yohualkis@DESKTOP-FKD43PD>
2025-09-01 00:31:52 -0400

    Se actualizo el TopBarGenerico para que usara el material de androidx.compose.material y no el de androidx.compose.material3

--- app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/ui/presentation/composables/TopBarGenerica.kt
+++ app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/ui/presentation/composables/TopBarGenerica.kt
@@ -1,17 +1,21 @@
 package com.ucne.yohualkis_tejada_p1_ap2.ui.presentation.composables
 
-import androidx.compose.foundation.layout.Row
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import com.ucne.yohualkis_tejada_p1_ap2.R
 
 @Composable
 fun TopBarGenerica(
@@ -19,35 +23,30 @@ fun TopBarGenerica(
     titulo: String,
     backButtonVisible: Boolean = true
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        if (backButtonVisible) {
-            IconButton(
-                onClick = { goBack() },
-            ) {
-                Icon(
-                    Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = ""
-                )
+    TopAppBar(
+        title = {
+            Text(
+                text = titulo,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        },
+        navigationIcon = if (backButtonVisible) {
+            {
+                IconButton(
+                    onClick = { goBack() }
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.atras)
+                    )
+                }
             }
-        } else {
-            IconButton(
-                onClick = { },
-                enabled = false
-            ) {
-                Icon(
-                    Icons.Default.Home,
-                    contentDescription = ""
-                )
-            }
-        }
-        Text(
-            text = titulo,
-            fontSize = 22.sp,
-            fontWeight = FontWeight.SemiBold
-        )
-    }
+        } else null,
+        backgroundColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        elevation = 4.dp,
+    )
 }
 
 @Preview(showBackground = true)
