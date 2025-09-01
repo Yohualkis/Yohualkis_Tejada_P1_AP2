4e903e6dd72b227bfd641e8328fb25e7fe9564a4
Yohualkis <Yohualkis@DESKTOP-FKD43PD>
2025-09-01 00:36:21 -0400

    Se eliminaron los imports innecesesarios

--- app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/ui/presentation/composables/TopBarGenerica.kt
+++ app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/ui/presentation/composables/TopBarGenerica.kt
@@ -1,20 +1,22 @@
 package com.ucne.yohualkis_tejada_p1_ap2.ui.presentation.composables
 
 //noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.material.Icon
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.IconButton
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.Text
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.ui.Modifier
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ucne.yohualkis_tejada_p1_ap2.R
 
 @Composable
