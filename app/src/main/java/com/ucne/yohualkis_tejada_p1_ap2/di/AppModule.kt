5ae8de301f31b3e9037aff0f720ddf5f9bd68a18
Yohualkis <Yohualkis@DESKTOP-FKD43PD>
2025-08-31 20:06:41 -0400

    Se optimizaron los imports en AppModule.kt

--- app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/di/AppModule.kt
+++ app/src/main/java/com/ucne/yohualkis_tejada_p1_ap2/di/AppModule.kt
@@ -3,8 +3,7 @@ package com.ucne.yohualkis_tejada_p1_ap2.di
 import android.content.Context
 import androidx.room.Room
 import com.ucne.yohualkis_tejada_p1_ap2.data.local.database.Parcial1Db
-import dagger.Module
-import dagger.Provides
+import dagger.*
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
